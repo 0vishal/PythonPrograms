ee4d7b7f6bac3575e705e398141febdf0c507884
0vishal <vishalsalaskar72@gmail.com>
2021-04-28 09:15:48 +0530

    Program to print username with string

---
+++ Username.py
@@ -0,0 +1,7 @@
+#To get the username with string
+username = input("Enter the value: ")
+
+if len(username) < 3:
+    print("Username should have minimum 3 characters")
+else:
+    print("Hello", username, "How are You ?") 
