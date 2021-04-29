926ff4be3e9c50188610254f6894d1163b69c28a
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 17:32:04 +0530

    [Vishal] Refactor : Added Try Except block for valid input and Program head structure

--- Flip.py
+++ Flip.py
@@ -1,9 +1,18 @@
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : Flip a coin and get Head Tail percentage 
+
 import random
 #To get the head tail percentage the number of times a coin is flipped
 head=0
 tail=0
 
-number = int(input("Number of times to flip a coin"))
+try:
+    number = int(input("Number of times to flip a coin"))
+except ValueError:
+    print("Enter correct number")
 
 if number < 0:
     print("Enter a positive integer")
