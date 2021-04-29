4cd3f6518801f0adf5aaf433b526fbcb93892100
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 17:33:07 +0530

    [Vishal] Refactor : Added Try Except block for valid input and Program Structure

--- Leap.py
+++ Leap.py
@@ -1,13 +1,25 @@
-#To check a year is leap year or not
-year = int(input("Enter a year: "))
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : To check a year is leap year or not 
 
-if (year % 4) == 0:
-   if (year % 100) == 0:
-       if (year % 400) == 0:
-           print(year, "is a leap year")
-       else:
-           print(year, "is not  a leap year")
-   else:
-       print(year, "is a leap year")
-else:
-   print(year, "is not  a leap year")
+#To check a year is leap year or not
+try:
+    year = int(input("Enter a year: "))
+except ValueError:
+    print("Enter a correct year")    
+
+try:
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print(year, "is a leap year")
+            else:
+                print(year, "is not  a leap year")
+        else:
+            print(year, "is a leap year")
+    else:
+        print(year, "is not  a leap year")
+except NameError as e:
+    print(e)        
