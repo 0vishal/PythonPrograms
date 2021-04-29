9eff419b02222b2cdb2e0ada2d454190d5001458
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 17:34:36 +0530

    [Vishal] Refactor : Added Try Except block for valid input and Program Structure

--- Primefactor.py
+++ Primefactor.py
@@ -1,8 +1,19 @@
-#To get the prime factor of the numbergit git git 
-number = int(input("Enter the value of N"))
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : To get the prime factor of a number
 
-for x in range(2,number):
-    while(number%x==0):
-        print(x)
-        number=number/x
-        
+#To get the prime factor of the number
+try:
+    number = int(input("Enter the value of N"))
+except ValueError:
+    print("Enter correct value")
+
+try:    
+    for x in range(2,number):
+        while(number%x==0):
+            print(x)
+            number=number/x
+except NameError as e:
+    print(e)        
