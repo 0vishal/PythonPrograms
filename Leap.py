cf84f5ef6940770988a45057d8666db875a0706c
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 18:37:29 +0530

    [Vishal] Refactor : Added Try Except block for valid input and Program Structure

--- Leap.py
+++ Leap.py
@@ -1,8 +1,10 @@
+'''
 @Author: Vishal Salaskar
 @Date: 2021-02-29 
 @Last Modified by: Vishal Salaskar
 @Last Modified time: 2021-02-29 
 @Title : To check a year is leap year or not 
+'''
 
 #To check a year is leap year or not
 try:
