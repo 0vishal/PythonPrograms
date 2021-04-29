37e8b95a9059bd3c3ac644464e81febb9fc3143a
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 17:26:49 +0530

    [Vishal] Refactor : Added Try Except block for valid input and function

--- Username.py
+++ Username.py
@@ -1,5 +1,14 @@
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : To Print USername with String 
+
 #To get the username with string
-username = input("Enter the value: ")
+try:
+    username = input("Enter the value: ")
+except ValueError:
+    print("Enter correct username in string")
 
 if len(username) < 3:
     print("Username should have minimum 3 characters")
