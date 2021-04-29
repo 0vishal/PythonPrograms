67f803f2ffcf19643d9d462a5c8cddd0eadcc62d
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 17:34:11 +0530

    [Vishal] Refactor : Added Try Except block for valid input and Program Structure

--- Harmonic.py
+++ Harmonic.py
@@ -1,11 +1,21 @@
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : To get the Nth Harmonic number
+
 #To get the Nth Harmonic number
 from fractions import Fraction
 
-number = int(input("Enter the power of N"))
-
+try:
+    number = int(input("Enter the power of N"))
+except ValueError:
+    print("Enter the positive value")
 x=1
 series=1
-for x in range(1,number+1):
-    series=Fraction(1/x)
-print(series)
- 
+try:
+    for x in range(1,number+1):
+        series=Fraction(1/x)
+    print(series)
+except NameError as e:
+    print(e) 
