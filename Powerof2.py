4fd4a7310e32826b05c35cd3114ecf8fa8720649
0vishal <vishalsalaskar72@gmail.com>
2021-04-29 17:33:45 +0530

    [Vishal] Refactor : Added Try Except block for valid input and Program Structure

--- Powerof2.py
+++ Powerof2.py
@@ -1,13 +1,25 @@
+@Author: Vishal Salaskar
+@Date: 2021-02-29 
+@Last Modified by: Vishal Salaskar
+@Last Modified time: 2021-02-29 
+@Title : To get the power of N  
+
 #To get the power of N
-number = int(input("Enter the power of N"))
+try: 
+    number = int(input("Enter the power of N"))
+except ValueError:
+    print("Enter a correct positive number")    
 
 power = 1
 N = 2
-if 0 < number < 31:
-    for x in range(0,number):
-        power = (power*N)
-        print(power)
-else:
-    print("Power of 2 overflows")        
+try:
+    if 0 < number < 31:
+        for x in range(0,number):
+            power = (power*N)
+            print(power)
+    else:
+        print("Power of 2 overflows") 
+except NameError as e:
+    print(e)               
 
     
